d3ef94342fd9f2653d9901a1b25de164e1263d21
qafarlicavid <javidig@code.edu.az>
2022-07-28 21:28:27 +0400

    Properties created in Blog class and BlogStatus enum created

---
+++ BlogManagement/BlogManagement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+    class Blog
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public BlogStatus Status { get; set; }
+        public string[] Tags { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public Blog(string title, string description, BlogStatus status, params string[] tags)
+        {
+            Title = title;
+            Description = description;
+            Status = status;
+            Tags = tags;
+        }
+    }
+    enum BlogStatus
+    {
+        Sended = 0,
+        Approved = 1,
+        Rejected = 2,
+    }
+}
